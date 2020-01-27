d8d2043686441fda601d4417fb03801238de12a3
Olivverde <dele19270@uvg.edu.gt>
2020-01-26 20:25:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mylab02_trainingapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mylab02_trainingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
